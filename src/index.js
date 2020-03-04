1feefdd25ae885956f45ca48d66288eda23961d4
David Trussler <mail@davidtrussler.net>
2020-03-04 17:23:07 +0000

    Adds reference to Yeast Sass files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '@bower_components/yeast/assets/test.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
